316ab387d0a25aa000660ba9f6d262e9568780f8
Nepochatova Polina <nepochatova.polina@unicyb.kiev.ua>
2020-09-22 20:27:27 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by polina on 22.09.2020.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by polina on 22.09.2020.
+//
+
+#ifndef LABORATORIES_3_MAIN_H
+#define LABORATORIES_3_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //LABORATORIES_3_MAIN_H
